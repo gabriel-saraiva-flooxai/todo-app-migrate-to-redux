77caae8db0b653f3e031fb586cc1e5b74e1176b1
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-30 15:03:21 -0300

    feat: add todo

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -3,7 +3,7 @@ import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux'
-import { changeDescription, search } from "./todoActions";
+import { add, changeDescription, search } from "./todoActions";
 
 class todoForm extends Component{
     constructor(props) {
@@ -16,14 +16,18 @@ class todoForm extends Component{
     }
 
     keyHandler(e) {
+        const { add, search, description } = this.props
+
         if (e.key === 'Enter') {
-            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+            e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
             this.props.handleClear()
         }
     }
 
     render() {
+        const { add, search, description } = this.props
+
         return (
             <div role="form" className="todoForm">
 
@@ -39,9 +43,9 @@ class todoForm extends Component{
 
             <Grid cols='12 3 2'>
                 <IconButton style='primary' icon='plus'
-                    onClick={this.props.handleAdd} />
+                    onClick={() => add(description)} />
                 <IconButton style='info' icon='search'
-                    onClick={this.props.handleSearch} />
+                    onClick={() => search()} />
                 <IconButton style='default' icon='close'
                     onClick={this.props.handleClear} />
             </Grid>
@@ -53,5 +57,5 @@ class todoForm extends Component{
 
 const mapStateToProps = state => ({ description: state.todo.description})
 const mapDispatchToProps = dispatch =>
-    bindActionCreators({ changeDescription, search }, dispatch)
+    bindActionCreators({ add, changeDescription, search }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(todoForm)
