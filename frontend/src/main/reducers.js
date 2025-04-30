c90fa14567a0a853df50c5391cb534e9a7b086ac
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-30 14:20:48 -0300

    feat: configure Redux store and Provider in the application

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,22 +1,8 @@
 import { combineReducers } from "redux";
+import todoReducer from "../todo/todoReducer";
 
 const rootReducer = combineReducers({
-    todo: () => ({
-        description: 'Ler livros',
-        list: [{
-            _id: 1,
-            description: 'Pagar fatura do cartão',
-            done: false
-        }, {
-            _id: 2,
-            description: 'Reunião com equipe',
-            done: true
-        }, {
-            _id: 3,
-            description: 'Consulta medica',
-            done: false
-        }]
-    })
+    todo: todoReducer
 })
 
 export default rootReducer;
